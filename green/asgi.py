fa61411bdf7c6b427b919271874a6b12a28705b4
Free-Ralph <okosachideragia@gmail.com>
2023-08-27 15:06:24 +0100

    corrections

--- green/asgi.py
+++ green/asgi.py
@@ -4,14 +4,9 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from chat.routing import websocket_urlpatterns
-from decouple import config
 
-DEBUG = config('DEBUG').lower() in ('true', 't', '1')
 
-if DEBUG:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'green.settings.dev')
-else:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'green.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'green.settings.prod')
 
 application = ProtocolTypeRouter(
     {
